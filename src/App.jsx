a998f7de2e639f7cffa4ba872151d6fc16538dcd
Anh Duc <skytam1234@gmail.com>
2025-11-25 12:38:21 +0700

    fix SearchPage

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter } from "react-router";
 import AppRoute from "./components/AppRouter";
 
 function App() {
-  return <AppRoute />;
+    return <AppRoute />;
 }
 
 export default App;
--- src/layouts/DefaultLayout/index.jsx
+++ src/layouts/DefaultLayout/index.jsx
@@ -5,21 +5,21 @@ import LeftSidebar from "./components/LeftSidebar";
 import RightSidebar from "./components/RightSidebar";
 
 function DefaultLayout() {
-  return (
-    <div className="min-h-screen bg-background">
-      <Header />
+    return (
+        <div className="min-h-screen bg-layout-background">
+            <Header />
 
-      <div className="flex h-screen overflow-hidden">
-        <LeftSidebar />
-        <main className=" w-full sm:w-[640px] bg-background mx-auto min-w-0 mt-20 sm:rounded-t-2xl sm:border-2 shadow-sm z-60 overflow-hidden ">
-          <div className="fixed inset-0 bg-transparent overflow-y-auto z-61 mt-20 ">
-            <Outlet />
-          </div>
-        </main>
-        <RightSidebar />
-      </div>
-      <Navigator />
-    </div>
-  );
+            <div className="flex h-screen overflow-hidden">
+                <LeftSidebar />
+                <main className=" w-full sm:w-[640px] bg-layout-main  mx-auto min-w-0 mt-20 sm:rounded-t-2xl sm:border-2 shadow-sm z-60 overflow-hidden ">
+                    <div className="fixed inset-0 bg-transparent overflow-y-auto z-61 mt-20 ">
+                        <Outlet />
+                    </div>
+                </main>
+                <RightSidebar />
+            </div>
+            <Navigator />
+        </div>
+    );
 }
 export default DefaultLayout;
